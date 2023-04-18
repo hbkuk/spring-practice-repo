3a31852d7b255277bbb2590963b8703c67c15549
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-18 10:27:53 +0900

    feat: Controller 인터페이스를 구현한 LoginController 클래스 작성

---
+++ src/main/java/com/springbook/view/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.springbook.view.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.springbook.biz.user.UserVO;
+import com.springbook.biz.user.impl.UserDAO;
+
+public class LoginController implements Controller {
+
+	@Override
+	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) {
+		System.out.println("로그인 처리");
+		
+		String id = request.getParameter("id");
+		String password = request.getParameter("password");
+		
+		UserVO vo = new UserVO();
+		vo.setId(id);
+		vo.setPassword(password);
+		
+		UserDAO userDao = new UserDAO();
+		UserVO user = userDao.getUser(vo);
+		
+		if( user != null ) {
+			return "getBoardList.do";
+		} else {
+			return "login";
+		}
+	}
+}
