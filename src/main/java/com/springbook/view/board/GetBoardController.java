b3d230c8178dee2a8b433b6f10c0de7fd153cf7c
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-18 14:51:16 +0900

    fix: 각 Controller의 인터페이스 구현체 변경
    스프링 MVC 적용을 위한 스프링에서 제공하는 Controller 인터페이스로 변경했습니다.

--- src/main/java/com/springbook/view/board/GetBoardController.java
+++ src/main/java/com/springbook/view/board/GetBoardController.java
@@ -2,29 +2,31 @@ package com.springbook.view.board;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
 
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
-import com.springbook.view.controller.Controller;
 
 public class GetBoardController implements Controller {
 
 	@Override
-	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("글 상세 조회");
 		
 		String seq = request.getParameter("seq");
-
+		
 		BoardVO vo = new BoardVO();
 		vo.setSeq(Integer.parseInt(seq));
 		
 		BoardDAO boardDAO = new BoardDAO();
 		BoardVO board = boardDAO.getBoard(vo);
 		
-		HttpSession session = request.getSession();
-		session.setAttribute("board", board);
-		return "getBoard";
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("board", board);
+		mav.setViewName("getBoard");
+		return mav;
 	}
 
 }
--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -3,14 +3,16 @@ package com.springbook.view.user;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
-import com.springbook.view.controller.Controller;
 
 public class LoginController implements Controller {
 
 	@Override
-	public String handlerRequest(HttpServletRequest request, HttpServletResponse response) {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		System.out.println("로그인 처리");
 		
 		String id = request.getParameter("id");
@@ -23,10 +25,13 @@ public class LoginController implements Controller {
 		UserDAO userDao = new UserDAO();
 		UserVO user = userDao.getUser(vo);
 		
+		ModelAndView mav = new ModelAndView();
+		
 		if( user != null ) {
-			return "getBoardList.do";
+			mav.setViewName("redirect:getBoardList.do");
 		} else {
-			return "login";
+			mav.setViewName("redirect:login.jsp");
 		}
+		return mav;
 	}
 }
