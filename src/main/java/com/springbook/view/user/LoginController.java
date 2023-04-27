5a88cdf755473b323bb8b3eb5731d23c72b41ad8
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-28 00:28:26 +0900

    refactor: User Controller 통합

--- src/main/java/com/springbook/view/user/LoginController.java
+++ src/main/java/com/springbook/view/user/LoginController.java
@@ -2,19 +2,26 @@ package com.springbook.view.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.springbook.biz.user.UserVO;
 import com.springbook.biz.user.impl.UserDAO;
 
 @Controller
 public class LoginController{
-
-	@RequestMapping("/login.do")
+	
+	@RequestMapping(value="/login.do", method=RequestMethod.GET )
+	public String loginView() {
+		System.out.println("로그인 화면으로 이동");
+		return "login.jsp";
+	}
+	
+	@RequestMapping(value="/login.do", method=RequestMethod.POST )
 	public String login(UserVO vo, UserDAO userDao, UserVO user) {
-		
+		System.out.println("로그인 인증 처리..");
 		if( user != null )
 			return "redirect:getBoardList.do";
-		
 		return "redirect:login.jsp";
 	}
 }
