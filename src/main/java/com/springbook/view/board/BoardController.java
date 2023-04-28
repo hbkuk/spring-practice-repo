01d5f2781cabf8c55b3f27f8decfae8a8dd7704d
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-28 15:17:23 +0900

    fix: 비즈니스 컴포넌트 사용을 위한 클래스 교체

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,13 +1,13 @@
 package com.springbook.view.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.servlet.ModelAndView;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
@@ -15,37 +15,36 @@ import com.springbook.biz.board.impl.BoardDAO;
 @SessionAttributes("board")
 public class BoardController {
 	
+	@Autowired
+	private BoardService boardService;
+	
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList( @RequestParam(value="searchCondition", defaultValue = "TITLE", required=false) String condition,
-								@RequestParam(value="searchKeyword", defaultValue = "", required=false) String keyword,
-								BoardVO vo, BoardDAO boardDao, Model model ) {
-		System.out.println( "검색 조건 : " + condition );
-		System.out.println( "검색 단어 : " + keyword );
-		model.addAttribute("boardList", boardDao.getBoardList(vo));
+	public String getBoardList(BoardVO vo, BoardDAO boardDao, Model model ) {
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp";
 	}
 	
 	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
-		model.addAttribute("board", boardDAO.getBoard(vo));
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo));
 		return "getBoard.jsp";
 	}
 	
 	@RequestMapping("/insertBoard.do")
-	public String insert(BoardVO vo, BoardDAO boradDAO) {
-		boradDAO.insertBoard(vo);
+	public String insert(BoardVO vo) {
+		boardService.insertBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 	
 	@RequestMapping("/updateBoard.do")
-	public String updaate(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO, ModelAndView mav){
-		boardDAO.updateBoard(vo);
+	public String updaate(@ModelAttribute("board") BoardVO vo){
+		boardService.updateBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 	
 	@RequestMapping("/deleteBoard.do")
-	public String delete(BoardVO vo, BoardDAO boardDAO) {
-		boardDAO.deleteBoard(vo);
+	public String delete(BoardVO vo) {
+		boardService.deleteBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 }
