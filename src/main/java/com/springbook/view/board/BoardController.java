c1279b0e98849709158a1d86a733265ae49e88fe
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-29 15:24:13 +0900

    feat: 파일 업로드 기능 추가
    파일 업로드 기능이 추가됨에 따라 코드가 추가되었습니다.
    - insertBoard.jsp에서 <form> 태그에 enctype 속성 및 속성값 multipart/form-data 이 추가되었습니다.
    - Command 객체가 수정되었습니다.
    - FileUpload 라이브러리가 추가되었습니다.
    - MultipartResolver 설정이 추가되었습니다.
    - Controller에서 파일 업로드를 처리하는 로직이 추가되었습니다.

--- src/main/java/com/springbook/view/board/BoardController.java
+++ src/main/java/com/springbook/view/board/BoardController.java
@@ -1,11 +1,15 @@
 package com.springbook.view.board;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
@@ -39,7 +43,13 @@ public class BoardController {
 	}
 	
 	@RequestMapping("/insertBoard.do")
-	public String insert(BoardVO vo) {
+	public String insert(BoardVO vo) throws IOException {
+		MultipartFile uploadFile = vo.getUploadFile();
+		if(!uploadFile.isEmpty()) {
+			String fileName = uploadFile.getOriginalFilename();
+			uploadFile.transferTo(new File("D:/" + fileName ));
+		}
+		
 		boardService.insertBoard(vo);
 		return "redirect:getBoardList.do";
 	}
